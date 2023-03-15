151ae8db71f39c85dca1bdaa3f6daf9f5ef7b467
Ryan Irelan <ryan@mijingo.com>
2023-03-15 11:18:33 -0500

    latest changes for course

---
+++ cms/config/element-api.php
@@ -0,0 +1,23 @@
+<?php
+
+use craft\elements\Entry;
+
+return [
+    'endpoints' => [
+        'news.json' => function() {
+            return [
+                'elementType' => Entry::class,
+                'criteria' => ['section' => 'marketNews'],
+                'transformer' => function(Entry $entry) {
+                    return [
+                        'title' => $entry->title,
+                        'url' => $entry->url,
+                        'postDate' => $entry->postDate->format('Y-m-d'),
+                        'articleSummary' => $entry->summaryText,
+                        'articleCopy' => $entry->richText
+                    ];
+                }
+            ];
+        }
+    ]
+];
